52b6b1d89f8328f0d0acd542bcc1cdb99184103b
droidfsck <droidfsck@gmail.com>
2012-08-28 01:44:44 -0700

    Initial status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Droidfsck::Application.config.secret_token = 'f2c03fde590edde0cf307eaa0b8a83aaed5af37a36b57b9c88d6b3164ee25acee7afa4c290dd19ec42d6a400c453038d29a683cc286d06365f34fe7f73ec0b22'
